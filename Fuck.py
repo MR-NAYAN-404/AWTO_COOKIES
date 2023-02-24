81bc08c998b2a062ecc5cf041f3d750b906eb861
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-24 11:34:46 +0600

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -3,9 +3,10 @@ import os,platform,time
 bitt=platform.architecture()[0]
 
 if bitt=="64bit":
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+	ip = requests.get("https://api.ipify.org").text
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Ip : {ip}');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import cooking
+    import MX.mahadi
 
 else:
 
