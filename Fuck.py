197440c104d4b10ba90338255d2ef41c8a004185
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-24 11:51:00 +0600

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,18 +1,23 @@
 import os,platform,time,requests,random
-P = "\x1b[1;97m"
-M = "\x1b[1;91m"
-H = "\x1b[1;92m"
-K = "\x1b[1;93m"
-B = "\x1b[1;94m"
-U = "\x1b[1;95m" 
-O = "\x1b[1;96m"
-N = "\x1b[0m"
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+my_color = [
+ M, H, K, B, U, O, N, A]
+warna = random.choice(my_color)
+wa = random.choice(my_color)
 nm = random.choice([m,k,h,u,b])
 bitt=platform.architecture()[0]
 
 if bitt=="64bit":
     ip = requests.get("https://api.ipify.org").text
-    os.system("clear");nm = random.choice([m,k,h,u,b]);print(f"[!]{nm} Your Device is 64 bit");time.sleep(1);print("\n\n[!] Your Ip : "+ip);time.sleep(1);print("\n\n[!] Your Python Version :");time.sleep(1);os.system("python --version")
+    os.system("clear");wa = random.choice(my_color);print(f"[!]{wa} Your Device is 64 bit");time.sleep(1);print("\n\n[!] Your Ip : "+ip);time.sleep(1);print("\n\n[!] Your Python Version :");time.sleep(1);os.system("python --version")
     time.sleep(5)
     import cooking
 
