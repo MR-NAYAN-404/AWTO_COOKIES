1b8d93b329761aeb9bd06149f0b9f3d41e79fb56
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-24 11:46:56 +0600

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,13 +1,21 @@
 import os,platform,time,requests
-
+P = "\x1b[1;97m"
+M = "\x1b[1;91m"
+H = "\x1b[1;92m"
+K = "\x1b[1;93m"
+B = "\x1b[1;94m"
+U = "\x1b[1;95m" 
+O = "\x1b[1;96m"
+N = "\x1b[0m"
+nm = random.choice([m,k,h,u,b])
 bitt=platform.architecture()[0]
 
 if bitt=="64bit":
     ip = requests.get("https://api.ipify.org").text
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Ip :'+ip);time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
-    time.sleep(2)
+    os.system("clear");nm = random.choice([m,k,h,u,b]);print(f"[!]{nm} Your Device is 64 bit");time.sleep(1);print("\n\n[!] Your Ip : "+ip);time.sleep(1);print("\n\n[!] Your Python Version :");time.sleep(1);os.system("python --version")
+    time.sleep(5)
     import cooking
 
 else:
 
-    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+    print("\nYOUR DEVICE 32 BIT NOT SUPPORT")
