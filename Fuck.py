853c29475537f65a7cb9196fad1736ba51228ab5
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-24 11:39:03 +0600

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -4,7 +4,7 @@ bitt=platform.architecture()[0]
 
 if bitt=="64bit":
     ip = requests.get("https://api.ipify.org").text
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Ip : {ip}');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Ip :'+ip);time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import cooking
 
