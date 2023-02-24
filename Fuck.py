ad9c1cb82d0e98c0c181db0c5eb251b1f6a51c23
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2023-02-24 11:30:40 +0600

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,63 +1,9 @@
 import os,platform,time
-P = '\x1b[1;97m'
-M = '\x1b[1;91m'
-H = '\x1b[1;92m'
-K = '\x1b[1;93m'
-B = '\x1b[1;94m'
-U = '\x1b[1;95m' 
-O = '\x1b[1;96m'
-N = '\x1b[0m'    
-Z = "\033[1;30m"
-sir = '\033[41m\x1b[1;97m'
-x = '\33[m' # DEFAULT
-m = '\x1b[1;91m' #RED +
-k = '\033[93m' # KUNING +
-xr = '\x1b[1;92m' # HIJAU +
-hh = '\033[32m' # HIJAU -
-u = '\033[95m' # UNGU
-kk = '\033[33m' # KUNING -
-b = '\33[1;96m' # BIRU -
-p = '\x1b[0;34m' # BIRU +
-RED = '\033[1;91m'
-WHITE = '\033[1;97m'
-GREEN = '\033[1;32m' #
-YELLOW = '\033[1;33m'
-BLUE = '\033[1;34m'
-ORANGE = '\033[1;35m'
-P = '\x1b[1;97m' # PUTIH
-M = '\x1b[1;91m' # MERAH
-H = '\x1b[1;92m' # HIJAU
-K = '\x1b[1;93m' # KUNING
-B = '\x1b[1;94m' # BIRU
-U = '\x1b[1;95m' # UNGU
-O = '\x1b[1;96m' # BIRU MUDA
-N = '\x1b[0m'    # WARNA MATI
-A = '\x1b[1;90m' # WARNA ABU ABU
-BN = '\x1b[1;107m' # BELAKANG PUTIH
-BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
-BP = '\x1b[1;105m' # BELAKANG PINK
-BB = '\x1b[1;104m' # BELAKANG BIRU
-BK = '\x1b[1;103m' # BELAKANG KUNING
-BH = '\x1b[1;102m' # BELAKANG HIJAU
-BM = '\x1b[1;101m' # BELAJANG MERAH
-BA = '\x1b[1;100m' # BELAKANG ABU ABU
-my_color = [
- P, M, H, K, B, U, O, N]
-warna = random.choice(my_color)
-warna1 = random.choice(my_color)
-warna2 = random.choice(my_color)
-warna3 = random.choice(my_color)
-warna4 = random.choice(my_color)
-warna5 = random.choice(my_color)
-warna6 = random.choice(my_color)
-gy = [YELLOW,GREEN]
-gyc = random.choice(gy)
+
 bitt=platform.architecture()[0]
 
 if bitt=="64bit":
-	wa = random.choice(my_color)
-	ip = requests.get("https://api.ipify.org").text
-    os.system('clear');print(f'➥➥➣{wa}Your Device is 64 bit');time.sleep(1);print(f'\n\n➥➥➣{wa} Your Python Version :');time.sleep(1);os.system('python --version')
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
     import cooking
 
